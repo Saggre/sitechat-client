40bca0bb2cf1f63bd5ff122caa8ba35505c1b300
Saggre <sakri.koskimies@hotmail.com>
2018-06-02 21:41:49 +0200

    Develop UI

--- public_html/js/sitechat.js
+++ public_html/js/sitechat.js
@@ -6,7 +6,7 @@
 // iframe (dynamic async)
 (function (d) {
     var iframe = d.createElement('iframe');
-    
+
     //For IE7 compatibility
     /*var idoc = iframe.contentWindow.document;
      idoc.open();e       
@@ -14,6 +14,7 @@
      idoc.close();*/
 
     iframe.src = 'http://localhost:1337/v1/client/saggre';
+    iframe.setAttribute("style", "width: 400px; height:500px; background-color: transparent; border: none; overflow: hidden;");
 
     d.body.appendChild(iframe);
 }(document));
